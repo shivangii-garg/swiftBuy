2bf7b247a6886717c6f626b78404153b4b383b55
shgarg <shgarg@axway.com>
2023-07-16 01:32:44 +0530

    body created

--- front-end/swift-buy/src/App.js
+++ front-end/swift-buy/src/App.js
@@ -1,3 +1,4 @@
+import Body from "./molecules/body";
 import Corousel from "./molecules/corousel";
 import HeadingBar from "./molecules/headingBar";
 import NavBar from "./molecules/navBar";
@@ -8,6 +9,7 @@ function App() {
       <HeadingBar />
       <NavBar /> {/* accidentally, pushed the NavBar code in Swif-3 branch */}
       <Corousel />
+      <Body />
       <h1>SwiftBuy</h1>
     </div>
   );
